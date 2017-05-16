b1c58d7638fc6f978558cf6f01673cc81b8ebb72
Mario <mario.saliba98@gmail.com>
2017-05-16 11:50:29 +0300

    adds basic route and html page to the bus route #30

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const homeHandler = require('./home.js');
 const staticHandler = require('./static.js');
+const busHandler = require('./bus.js');
 
 module.exports = [
   homeHandler,
   staticHandler,
+  busHandler,
 ];
